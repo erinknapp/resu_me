ad88dd4b47f6a48bc05087e8c1053001460cbf22
Erin Knapp <erin.knapp@gmail.com>
2022-03-28 19:09:27 -0400

    server woes

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost:27017/deep-thoughts',
+  process.env.MONGODB_URI || 'mongodb+srv://eknapp:peanutbutter1@cluster0.swna2.mongodb.net/resume?retryWrites=true&w=majority',
   {
     useNewUrlParser: true, 
     useUnifiedTopology: true,
