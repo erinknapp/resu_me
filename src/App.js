7479d66249018de855a31e378f4c8cd14b6d3be2
CBoensch <cboensch6505@gmail.com>
2022-03-30 12:49:07 -0400

    Stipe payment converted into donations link on Home.js

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+// import { ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client";
+// import { setContext } from "@apollo/client/link/context";
+
+import Home from "./pages/Home";
+import Pro from "./pages/Pro";
+import Light from "./pages/Light";
+
+// ***************************//
+// const httpLink = createHttpLink({
+// 	uri: "/graphql",
+// });
+
+// const authLink = setContext((_, { headers }) => {
+// 	const token = localStorage.getItem("id_token");
+// 	return {
+// 		headers: {
+// 			...headers,
+// 			authorization: token ? `Bearer ${token}` : "",
+// 		},
+// 	};
+// });
+
+// const client = new ApolloClient({
+// 	link: authLink.concat(httpLink),
+// 	cache: new InMemoryCache(),
+// });
+// ***************************//
+
+function App() {
+	return (
+		<div>
+			<BrowserRouter>
+				<Routes>
+					<Route path='/' element={<Home />} />
+					<Route path='/pro' element={<Pro />} />
+					<Route path='/light' element={<Light />} />
+				</Routes>
+			</BrowserRouter>
+		</div>
+	);
+}
+
+export default App;
